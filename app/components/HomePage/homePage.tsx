5b702a8efd4d164ceba4356eabcf038b58d1374d
Tony <tony@Emilys-MacBook-Pro.local>
2020-05-21 16:42:13 -0700

    added homePage component to app

---
+++ app/components/HomePage/homePage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Component} from 'react'; 
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to ReactionTime</h1>
+            </div>
+        )
+    }
+}
+
+export default HomePage; 
+
+
+

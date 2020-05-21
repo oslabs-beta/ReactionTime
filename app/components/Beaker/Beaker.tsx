c2f3b3966cd2d73fbcb6e5525a164516eabbe819
jayacados <jayacados@gmail.com>
2020-05-20 21:56:14 -0700

    Feature: SPA Routing
    Beaker component routes to four different cards upon click

--- app/components/Beaker/Beaker.tsx
+++ app/components/Beaker/Beaker.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+// import react router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// import routes for react router
+import { CreateRootTest } from "./CreateRootTest";
+import { PackageTest } from "./PackageTest";
+import { FallbackTest } from "./FallbackTest";
+import { SuspenseTest } from "./SuspenseTest";
+
 // Import stylesheet for flexbox grid
 import "./styles.css";
 
@@ -10,11 +19,6 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-
-// Import Box Component for Flexbox Config
-
-// Sandbox Test Code
-// https://codesandbox.io/s/busy-chatterjee-x6tek?file=/src/App.js:0-4021
 
 const useStyles = makeStyles({
   root: {
@@ -39,89 +43,120 @@ export default function SimpleCard() {
   // Rendering our Components Below
   return (
     // Begin 2x2
-    <div className="grid2x2">
-      {/* Begin Card 1 */}
-      <div className="box box1">
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              createRoot
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              Enables Concurrent Mode
-            </Typography>
-            <Typography variant="body2" component="p">
-              {`Replaces ReactDOM.render(<App />, rootNode) and enables Concurrent Mode.`}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small"> Code Snippet </Button>
-          </CardActions>
-        </Card>
-      </div>
-      {/* End Card 1 */}
-      {/* Begin Card 2 */}
-      <div className="box box2">
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              packageTest
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              Verifies React Experimental
-            </Typography>
-            <Typography variant="body2" component="p">
-              {`Concurrent Mode is only available in the experimental builds of React.`}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small"> Code Snippet </Button>
-          </CardActions>
-        </Card>
-      </div>
-      {/* End Card 2 */}
-      {/* Begin Card 3 */}
-      <div className="box box3">
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              fallbackTest
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              Ensures fallback component is present and defined.
-            </Typography>
-            <Typography variant="body2" component="p">
-              {`Concurrent Mode is only available in the experimental builds of React.`}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small"> Code Snippet </Button>
-          </CardActions>
-        </Card>
-      </div>
-      {/* End Card 3 */}
-      {/* Begin Card 4 */}
-      <div className="box box4">
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              suspenseTest
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              Mock test to determine fallback rendersin the DOM tree.
-            </Typography>
-            <Typography variant="body2" component="p">
-              {`This will take one argument, a child component that the user expects to suspend. As of now, the suspended component must act as a standalone component.`}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small"> Code Snippet </Button>
-          </CardActions>
-        </Card>
-      </div>
-      {/* End Card 4 */}
+    <Router>
+      <div className="grid2x2">
+        {/* Begin Card 1 */}
+        <div className="box box1">
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                createRoot
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Enables Concurrent Mode
+              </Typography>
+              <Typography variant="body2" component="p">
+                {`Replaces ReactDOM.render(<App />, rootNode) and enables Concurrent Mode.`}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" component={Link} to={"/CreateRootTest"}>
+                {" "}
+                Code Snippet{" "}
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
+        {/* End Card 1 */}
+        {/* Begin Card 2 */}
+        <div className="box box2">
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                packageTest
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Verifies React Experimental
+              </Typography>
+              <Typography variant="body2" component="p">
+                {`Concurrent Mode is only available in the experimental builds of React.`}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" component={Link} to={"/PackageTest"}>
+                {" "}
+                Code Snippet{" "}
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
+        {/* End Card 2 */}
+        {/* Begin Card 3 */}
+        <div className="box box3">
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                fallbackTest
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Ensures fallback component is present and defined.
+              </Typography>
+              <Typography variant="body2" component="p">
+                {`Concurrent Mode is only available in the experimental builds of React.`}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" component={Link} to={"/FallBackTest"}>
+                {" "}
+                Code Snippet{" "}
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
+        {/* End Card 3 */}
+        {/* Begin Card 4 */}
+        <div className="box box4">
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                suspenseTest
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                Mock test to determine fallback rendersin the DOM tree.
+              </Typography>
+              <Typography variant="body2" component="p">
+                {`This will take one argument, a child component that the user expects to suspend. As of now, the suspended component must act as a standalone component.`}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" component={Link} to={"/SuspenseTest"}>
+                {" "}
+                Code Snippet{" "}
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
+        {/* End Card 4 */}
 
-      {/* End 2x2 */}
-    </div>
+        {/* Switch Begin */}
+        <Switch>
+          <Route exact path="/CreateRootTest">
+            <CreateRootTest />
+          </Route>
+          <Route exact path="/FallbackTest">
+            <FallbackTest />
+          </Route>
+          <Route exact path="/PackageTest">
+            <PackageTest />
+          </Route>
+          <Route exact path="/SuspenseTest">
+            <SuspenseTest />
+          </Route>
+        </Switch>
+        {/* Switch End */}
+
+        {/* End 2x2 */}
+      </div>
+    </Router>
   );
 }
---
+++ app/components/Beaker/SuspenseTest.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SuspenseTest = () => {
+  return (
+    <div>
+      <h2>SuspenseTest</h2>
+      <h4> Display copy snippet for SuspenseTest here</h4>
+    </div>
+  );
+};

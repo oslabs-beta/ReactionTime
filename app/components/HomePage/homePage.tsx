a022bfe18b8ca93d00f26611170e58d113732461
Tony <tony@Emilys-MacBook-Pro.local>
2020-05-21 22:39:29 -0700

    img component
    added image component displaying reactiontime logo that christian had made in resource folder

--- app/components/HomePage/homePage.tsx
+++ app/components/HomePage/homePage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Component} from 'react'; 
 
+const reactionTimeLogo = require(".././resources/icons/512x512.png");
+
 class HomePage extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome to ReactionTime</h1>
+                <img src={reactionTimeLogo} alt="ReactionTime Logo"/>
             </div>
         )
     }

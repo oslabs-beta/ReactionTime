38dd725a4db8f4fadb71a3847bdd93dd05301824
Christian Hubbard <chrishubbard92@hotmail.com>
2020-05-21 20:47:56 -0700

    deleted redux from app. Cleaned out boiler plate and set new home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,15 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
-import './app.global.css';
+import Beaker from './components/Beaker'
 
-const store = configureStore();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
 document.addEventListener('DOMContentLoaded', () =>
   render(
     <AppContainer>
-      <Root store={store} history={history} />
+      <Beaker />
     </AppContainer>,
     document.getElementById('root')
   )

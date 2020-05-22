d0cb2330794991709a99fb33ea3cb17edf728df3
Tony <tony@Emilys-MacBook-Pro.local>
2020-05-22 15:55:36 -0700

    styling
    added styled components for title and reactionTime logo on homePage

--- app/components/HomePage/homePage.tsx
+++ app/components/HomePage/homePage.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import {Component} from 'react'; 
 
-
+import styled from 'styled-components';
 import Logo from "./logo.png";
+
+const Image = styled.img`
+    width: 30em;
+    height: 30em;
+    align-items: center;
+  
+`
+const Title = styled.h1`
+    text-align: center;
+`
+
 
 class HomePage extends Component {
     render() {
         return (
             <div>
-                    <h1>Welcome to ReactionTime</h1>
-                    <img src={Logo} alt="ReactionTime Logo"/>
+                <Title>Welcome to ReactionTime</Title>
+                <Image
+                 src={Logo} alt="ReactionTime Logo"
+                 >
+                 </Image>
             </div>
         )
     }

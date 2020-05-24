52df6f3dfce2309cee5cd3245a7d85d8a31af8d6
Christian Hubbard <chrishubbard92@hotmail.com>
2020-05-23 17:07:48 -0700

    Built test generators for each test

--- app/components/SuspenseTest.tsx
+++ app/components/SuspenseTest.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const SuspenseTest = () => {
+  const [name, setName] = useState("");
+  
+  let result: string;
+
+  const handleSubmit = (evt) => {
+      evt.preventDefault();
+      result = `
+      test('renders suspense fallback on a child component', () => {
+        const ComponentWrapper = (${name}) => {
+         return (
+            <React.Suspense fallback={'testFallback'}>
+              <${name} />
+            </React.Suspense>
+          );
+        };
+    
+        const wrapper = testingLibrary.render(<ComponentWrapper />);
+       
+        expect((wrapper.queryAllByText('testFallback')[0].textContent)).toMatch(/testFallback/);
+    })
+      `;
+
+      document.getElementById('123')?.append(result)
+      
+  }
+
   return (
     <div style={{paddingLeft: '80px'}}>
-      <h2>SuspenseTest</h2>
-      <h4> Display copy snippet for SuspenseTest here</h4>
+      <form onSubmit={handleSubmit}>
+      <label>
+        Please enter the name of your Suspenseful Component:
+        <input type="text"
+        value={name}
+        onChange={e => setName(e.target.value)} />
+      </label>
+      <input type="submit" value="Generate" />
+      </form>
+      <div id="123">
+      </div>
     </div>
   );
 };

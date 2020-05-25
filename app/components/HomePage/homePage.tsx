7cb4712bbc2535b4c7bdf6e60e42183b8ede1376
Tony <tony@Emilys-MacBook-Pro.local>
2020-05-25 16:04:27 -0700

    styling
    added google font and background color styling

--- app/components/HomePage/homePage.tsx
+++ app/components/HomePage/homePage.tsx
@@ -5,13 +5,12 @@ import './homePage.css';
 import logo from "./resources/logo.png";
 
 
-
 class HomePage extends Component {
     render() {
         return (
-            <div style={{backgroundColor: '#51EAEA', paddingLeft: '100px', paddingRight: '100px'}}>
-                <h1 className='heading' style={{textAlign: 'center', padding: '1rem'}}>Welcome to ReactionTime</h1>
-                    <h5 className='subtitle' style={{textAlign: 'center'}}>A SIMPLER WAY TO WRITE TESTS FOR REACT'S EXPERIMENTAL CONCURRENT MODE</h5>
+            <div style={{backgroundColor: '#51EAEA', height: '100%', paddingLeft: '100px', paddingRight: '100px'}}>
+                <h1 className='heading' style={{fontFamily: "Heebo, sans-serif", fontWeight: 'bold', color: 'white', textAlign: 'center', padding: '1rem'}}>Welcome to ReactionTime</h1>
+                    <h4 className='subtitle' style={{fontFamily: "Heebo, sans-serif", color: 'white', fontStyle: 'italic', textAlign: 'center'}}>A SIMPLER WAY TO WRITE TESTS FOR REACT'S EXPERIMENTAL CONCURRENT MODE</h4>
                 <br/>
                 <br/>
                 <img style={{display: 'block', marginLeft: "auto", marginRight: 'auto', width: '225px', height: '225px'}}
@@ -20,7 +19,7 @@ class HomePage extends Component {
                  </img>
                  <br/>
                  <br/>
-                 <p className='text' style={{textAlign: 'center', marginLeft: "auto", marginRight: 'auto' }}>
+                 <p className='text' style={{fontFamily: "Heebo, sans-serif", color: 'white', textAlign: 'center', marginLeft: "auto", marginRight: 'auto' }}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                     dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
                     torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
@@ -33,7 +32,7 @@ class HomePage extends Component {
                     placerat maximus. Pellentesque aliquam maximus risus, vel sed
                     vehicula.
                 </p>
-                <p className='text' style={{textAlign: 'center', marginLeft: "auto", marginRight: 'auto'}}> 
+                <p className='text' style={{fontFamily: "Heebo, sans-serif", color: 'white', textAlign: 'center', marginLeft: "auto", marginRight: 'auto'}}> 
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                     dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
                     torquent per conubia nostra, per inceptos himenaeos. Etiam tristique

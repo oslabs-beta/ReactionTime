6c809f5f259cb4ea01e82225217f0b29bada8ee6
Tony <tony@Emilys-MacBook-Pro.local>
2020-05-26 15:33:51 -0700

    material ui components
    added card, cardContent, and typography components from material ui for each step on how to set up reactionTime in setUp component

--- app/components/SetUp.tsx
+++ app/components/SetUp.tsx
@@ -1,9 +1,82 @@
 import React from "react";
 
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
 export const SetUp = () => {
     return (
         <div style={{paddingLeft: '80px'}}>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, earum? Impedit excepturi aperiam odio similique dicta! Sapiente tempora autem error perferendis adipisci voluptatem perspiciatis aut harum, voluptatum reprehenderit a dolor?</p>
+            <h1 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic'}}>How to Use ReactionTime</h1>
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            Install Testing Libraries
+                        </Typography>
+                        <Typography color="textSecondary">
+                            Install Jest and Enzyme
+                        </Typography>
+                        <Typography component="p">
+                    
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            Create New Files
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            Import Library
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            Import Enzyme
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            Download ReactionTime
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            Input Names
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            Copy & Paste
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            Enjoy You Test!
+                        </Typography>
+                    </CardContent>
+                </Card>
         </div>
     );
 };

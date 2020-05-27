33b2213f09c68bc40685bfaa8bb28f422fbc09c8
jhm304 <jhm304@gmail.com>
2020-05-27 16:00:28 -0700

    change color of nav bar and the main app

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -58,6 +58,7 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
+    backgroundColor: '#222831',
     webPreferences:
       process.env.NODE_ENV === 'development' || process.env.E2E_BUILD === 'true'
         ? {

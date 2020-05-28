f26aab1f6a5bc9cef1134a197bb29adfc7535adc
Tony <tony@Emilys-MacBook-Pro.local>
2020-05-27 21:14:44 -0700

    style changes
    made css changes for searchGlass component, set background color for card components, changed font style to italic, set font color

--- app/components/SearchGlass.tsx
+++ app/components/SearchGlass.tsx
@@ -1,34 +1,52 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
 export const SearchGlass = () => {
     return (
+       
         <div style={{paddingLeft: '80px'}}>
-            <h1 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic'}}>Further Reading</h1>
+            <h1 style={{fontFamily: "Heebo, sans-serif", color: '#EBEBEB'}}>Further Reading</h1>
 
-            <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic'}}>What is Concurrency?</h4>
-                <section style={{fontFamily: "Heebo, sans-serif"}}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cumque obcaecati ducimus saepe ipsam reprehenderit eveniet amet est sit! Provident qui porro officia fuga sapiente repudiandae minus laboriosam vitae nesciunt.</p>
-                    <a target="_blank" href="https://medium.com/swlh/what-is-react-concurrent-mode-46989b5f15da">Learn More About Concurrency</a>
-                </section>
-
-            <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic'}}>Setting Up Concurrent Mode</h4>
-            <section style={{fontFamily: "Heebo, sans-serif"}}>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
-                <a target="_blank" href="https://reactjs.org/docs/concurrent-mode-adoption.html">Set Up Concurrent Mode</a>
-            </section>
-
-            <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic'}}>More on Concurrent Mode</h4>
-            <section style={{fontFamily: "Heebo, sans-serif"}}>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
-                <a target="_blank" href="https://reactjs.org/docs/concurrent-mode-intro.html">Learn More About Concurrent Mode</a>
-            </section>
-
-            <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic'}}>What Can You Do with Concurrent Mode?</h4>
-            <section style={{fontFamily: "Heebo, sans-serif"}}>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
-                <a target="_blank" href="https://www.youtube.com/watch?v=V1Ly-8Z1wQA&t=1053s">See What is Possible</a>
-            </section>
-           
+                <Card style={{backgroundColor: '#303030'}}>
+                <CardContent>
+                    <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>What is Concurrency?</h4>
+                        <section style={{fontFamily: "Heebo, sans-serif"}}>
+                            <p style={{color: '#EBEBEB'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cumque obcaecati ducimus saepe ipsam reprehenderit eveniet amet est sit! Provident qui porro officia fuga sapiente repudiandae minus laboriosam vitae nesciunt.</p>
+                            <a style={{color: '#3F51B5', fontStyle: 'italic'}} target="_blank" href="https://medium.com/swlh/what-is-react-concurrent-mode-46989b5f15da">Learn More About Concurrency</a>
+                        </section>
+                </CardContent>
+                </Card>
+                <br/>
+                <Card style={{backgroundColor: '#303030'}}>
+                <CardContent>
+                    <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>Setting Up Concurrent Mode</h4>
+                    <section style={{fontFamily: "Heebo, sans-serif"}}>
+                        <p style={{color: '#EBEBEB'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
+                        <a style={{color: '#3F51B5', fontStyle: 'italic', fontWeight: 700}}target="_blank" href="https://reactjs.org/docs/concurrent-mode-adoption.html">Set Up Concurrent Mode</a>
+                    </section>
+                </CardContent>
+                </Card>
+                <br/>
+                <Card style={{backgroundColor: '#303030'}}>
+                <CardContent>
+                    <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>More on Concurrent Mode</h4>
+                    <section style={{fontFamily: "Heebo, sans-serif"}}>
+                        <p style={{color: '#EBEBEB'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
+                        <a style={{color: '#3F51B5', fontStyle: 'italic', fontWeight: 700}}target="_blank" href="https://reactjs.org/docs/concurrent-mode-intro.html">Learn More About Concurrent Mode</a>
+                    </section>
+                </CardContent>
+                </Card>
+                <br/>
+                <Card style={{backgroundColor: '#303030'}}>
+                <CardContent>
+                    <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>What Can You Do with Concurrent Mode?</h4>
+                    <section style={{fontFamily: "Heebo, sans-serif"}}>
+                        <p style={{color: '#EBEBEB'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
+                        <a style={{color: '#3F51B5', fontStyle: 'italic', fontWeight: 700}}target="_blank" href="https://www.youtube.com/watch?v=V1Ly-8Z1wQA&t=1053s">See What is Possible</a>
+                    </section>
+                </CardContent>
+                </Card>
         </div>
     );
 };

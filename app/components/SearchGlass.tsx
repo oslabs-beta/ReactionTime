9ca12050969df0f7e80b33db1d14dba7e70aca95
Tony <tony@Emilys-MacBook-Pro.local>
2020-05-27 21:53:33 -0700

    component change
    change h4 component to h3 inside of cardContent component

--- app/components/SearchGlass.tsx
+++ app/components/SearchGlass.tsx
@@ -10,7 +10,7 @@ export const SearchGlass = () => {
 
                 <Card style={{backgroundColor: '#303030'}}>
                 <CardContent>
-                    <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>What is Concurrency?</h4>
+                    <h3 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>What is Concurrency?</h3>
                         <section style={{fontFamily: "Heebo, sans-serif"}}>
                             <p style={{color: '#EBEBEB'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cumque obcaecati ducimus saepe ipsam reprehenderit eveniet amet est sit! Provident qui porro officia fuga sapiente repudiandae minus laboriosam vitae nesciunt.</p>
                             <a style={{color: '#3F51B5', fontStyle: 'italic'}} target="_blank" href="https://medium.com/swlh/what-is-react-concurrent-mode-46989b5f15da">Learn More About Concurrency</a>
@@ -20,7 +20,7 @@ export const SearchGlass = () => {
                 <br/>
                 <Card style={{backgroundColor: '#303030'}}>
                 <CardContent>
-                    <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>Setting Up Concurrent Mode</h4>
+                    <h3 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>Setting Up Concurrent Mode</h3>
                     <section style={{fontFamily: "Heebo, sans-serif"}}>
                         <p style={{color: '#EBEBEB'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
                         <a style={{color: '#3F51B5', fontStyle: 'italic', fontWeight: 700}}target="_blank" href="https://reactjs.org/docs/concurrent-mode-adoption.html">Set Up Concurrent Mode</a>
@@ -30,7 +30,7 @@ export const SearchGlass = () => {
                 <br/>
                 <Card style={{backgroundColor: '#303030'}}>
                 <CardContent>
-                    <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>More on Concurrent Mode</h4>
+                    <h3 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>More on Concurrent Mode</h3>
                     <section style={{fontFamily: "Heebo, sans-serif"}}>
                         <p style={{color: '#EBEBEB'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
                         <a style={{color: '#3F51B5', fontStyle: 'italic', fontWeight: 700}}target="_blank" href="https://reactjs.org/docs/concurrent-mode-intro.html">Learn More About Concurrent Mode</a>
@@ -40,7 +40,7 @@ export const SearchGlass = () => {
                 <br/>
                 <Card style={{backgroundColor: '#303030'}}>
                 <CardContent>
-                    <h4 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>What Can You Do with Concurrent Mode?</h4>
+                    <h3 style={{fontFamily: "Heebo, sans-serif", fontStyle: 'italic', color: '#EBEBEB'}}>What Can You Do with Concurrent Mode?</h3>
                     <section style={{fontFamily: "Heebo, sans-serif"}}>
                         <p style={{color: '#EBEBEB'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus vel sunt magni. Nulla natus dolorum totam unde enim. Perspiciatis facilis et veritatis porro consectetur? Ipsa id eos odit facilis</p>
                         <a style={{color: '#3F51B5', fontStyle: 'italic', fontWeight: 700}}target="_blank" href="https://www.youtube.com/watch?v=V1Ly-8Z1wQA&t=1053s">See What is Possible</a>
